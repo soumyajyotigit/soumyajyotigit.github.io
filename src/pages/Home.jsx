937c2b04483f6f05cc5ed17a57115039ffe2f9ef
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2025-07-14 02:00:17 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h2 className="text-2xl font-bold mb-4">Welcome to My Portfolio</h2>
       <p>
         I’m a MERN Stack Developer skilled in Docker, Kubernetes, and scalable web
-        architecture. Explore my work!
+        architecture. Explore my work hehe!
       </p>
       <GitHubStats />
     </div>
